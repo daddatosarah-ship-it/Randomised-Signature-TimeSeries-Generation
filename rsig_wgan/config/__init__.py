3243f32ae0bcaf7ae5909aea36b40f6c20640014
daddatosarah <daddatosarah@gmail.com>
2025-12-12 13:15:39 +0100

    commenti

--- rsig_wgan/config/__init__.py
+++ rsig_wgan/config/__init__.py
@@ -9,8 +9,10 @@ def load_config() -> dict[str, Any]:
     return: 
         Dictionary with the config
     """
+    
+    #vediamo se esiste nella directory un file chiamato config.yml
     try:
-        config = OmegaConf.load(Path(__file__).parent / "config.yml")
+        config = OmegaConf.load(Path(__file__).parent / "config.yml")   #OmegaConf libreria più evoluta rispetto a yaml,permette di accedere ai campi come attributi 
         return config
     except FileNotFoundError:
         print(f"Error: The file at config.yml was not found.")
@@ -19,7 +21,8 @@ def load_config() -> dict[str, Any]:
         print(f"Error reading YAML file: {e}")
         return None
     
-activation_mapping = {"Sigmoid": nn.Sigmoid(), "Tanh": nn.Tanh()}
+activation_mapping = {"Sigmoid": nn.Sigmoid(), "Tanh": nn.Tanh()}   #Serve per mappare un nome stringa preso dal file config.yml a una vera attivazione PyTorch
     
-__all__ = ["load_config", "activation_mapping"]
+__all__ = ["load_config", "activation_mapping"]   #quando qualcuno fa: from rsig_wgan.config import * verranno importati solo questi due simboli
+
 
