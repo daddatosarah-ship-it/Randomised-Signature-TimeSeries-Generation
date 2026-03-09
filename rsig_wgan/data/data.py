afa56ec48d532981859d764ee6de8671417c21a1
daddatosarah <daddatosarah@gmail.com>
2026-03-09 17:31:11 +0100

    Update data.py

--- rsig_wgan/data/data.py
+++ rsig_wgan/data/data.py
@@ -37,7 +37,7 @@ class BrownianMotion(Data):
         self.drift = drift
         self.std = std
         self.dim = dim
-        self.h = T / n_lags
+        self.h = 1
         self.scaler = IDScaler()
 
     def generate(self, samples: int) -> torch.tensor:
