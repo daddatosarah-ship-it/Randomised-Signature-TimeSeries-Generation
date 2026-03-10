ee8e973cff79799f0044dffa90b17be0cb4afd9b
daddatosarah <daddatosarah@gmail.com>
2026-03-10 12:06:44 +0100

    Update main.py

--- rsig_wgan/generator_unconditional/main.py
+++ rsig_wgan/generator_unconditional/main.py
@@ -13,8 +13,7 @@ from evaluation import *
 import sys
 sys.path.append('/content/Randomised-Signature-TimeSeries-Generation/rsig_wgan/data')
 from data import *
-sys.path.append('/content/Randomised-Signature-TimeSeries-Generation/rsig_wgan/data')
-from data.utils import *
+from utils_data import *
 
 sys.path.append('/content/Randomised-Signature-TimeSeries-Generation/rsig_wgan/config')
 from config import *
